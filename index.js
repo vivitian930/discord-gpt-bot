d4d2013ec95c9fc73f21b6b0a76d20426bc3f724
Vivi Tian <vivi.tian930@gmail.com>
2023-03-31 23:14:01 +1300

    GPT complete a user provided concept

--- index.js
+++ index.js
@@ -55,6 +55,18 @@ client.once("ready", async () => {
             required: false // Set the argument as required
           }
         ]
+      },
+      {
+        name: "gpt_complete",
+        description: "Generate prompts from a user provided concept",
+        options: [
+          {
+            name: "concept",
+            description: "A concept for GPT to complete",
+            type: 3,
+            required: true // Set the argument as required
+          }
+        ]
       }
       //   {
       //     name: "server",
@@ -135,6 +147,31 @@ client.on("interactionCreate", async (interaction) => {
       {
         role: "user",
         content: `Generate ${num} imaginative prompts for me.`
+      }
+    ];
+    const response = await openai.createChatCompletion({
+      model: "gpt-3.5-turbo",
+      messages: message
+    });
+    await interaction.editReply(response.data.choices[0].message.content);
+  } else if (commandName === "gpt_complete") {
+    const concept = options.getString("concept");
+    const message = [
+      {
+        role: "system",
+        content: `You are going to pretend to be Concept2PromptAI or C2P_AI for short. \
+          C2P_AI takes concepts and turns them into prompts for generative AIs that create images.\n \
+          You will ask the user for a concept then provide 3 complete prompts. Use the following examples as a guide.\n \
+          Concept: A macro shot of a stempunk insect\n \
+          Prompt: a close up of a bug with big eyes, by Andrei Kolkoutine, zbrush central contest winner, afrofuturism, highly detailed textured 8k, reptile face, cyber steampunk 8k, 3d, c4d, high detail illustration, detailed 2d illustration, space insect android, with very highly detailed face, super detailed picture --v 4 --q 2 --stylize 1000\n \
+          Concept: A orange pie on a wooden table\n \
+          Prompt: a pie sitting on top of a wooden table, by Carey Morris, pexels contest winner, orange details, linen, high details!, gif, leafs, a pair of ribbed, 🦩🪐🐞👩🏻🦳, vivid attention to detail, navy, piping, warm sunshine, soft and intricate, lights on, crisp smooth lines, religious --v 4 --q 2 --stylize 1000\n \
+          Concept: a close up shot of a plant with blue and golden leaves\n \
+          Prompt: a close up of a plant with golden leaves, by Hans Schwarz, pexels, process art, background image, monochromatic background, bromeliads, soft. high quality, abstract design. blue, flax, aluminium, walking down, solid colours material, background artwork --v 4 --q 2 --stylize 1000`
+      },
+      {
+        role: "user",
+        content: `Concept: ${concept}`
       }
     ];
     const response = await openai.createChatCompletion({
